f4d6693ca40f271a242f410839fa2f84705a5b35
Fulya <fulya.ertay@gmail.com>
2023-05-08 15:30:07 +0300

    first init

---
+++ 2-code-reuse-in-react/challenge-3/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Menu from "./Menu"
+import Favorite from "./Favorite"
+import Toggler from "./Toggler"
+
+function App() {
+    return (
+        <div>
+            <Toggler render={({on, toggle}) => {
+                return (
+                    <Menu on={on} toggle={toggle}/>
+                )
+            }}/>
+            <hr />
+            <Favorite />
+        </div>
+    )
+}
+
+export default App
